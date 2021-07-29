8da0e8ba0c765726157c8209feb8e9df142327d5
J7W1 <55218859+18ec086@users.noreply.github.com>
2021-07-29 14:51:56 +0900

    Update RGB.ino

--- RGB.ino
+++ RGB.ino
@@ -2,36 +2,63 @@ int LED_R = 9;   // デジタルピン9番に赤色LEDを割り当てる(変数)
  int LED_G = 10;  // デジタルピン10番に緑色LEDを割り当てる(変数)
  int LED_B = 11;  // デジタルピン11番に青色LEDを割り当てる(変数)
  int WAIT = 500;      // 待ち時間の指定
-void red(int r, int g,int b,int wait){
-   digitalWrite(r, LOW);
-   digitalWrite(g, HIGH);   // 赤色LEDを点灯しその他を消灯
-   digitalWrite(b, HIGH);
-   delay(wait);
+ float cel2=0.0;
+void red(){
+   digitalWrite(LED_R, LOW);
+   digitalWrite(LED_G, HIGH);   // 赤色LEDを点灯しその他を消灯
+   digitalWrite(LED_B, HIGH);
+   delay(WAIT);
 }
-void blue(int r, int g,int b,int wait){
-  digitalWrite(r, HIGH);
-   digitalWrite(g, LOW);  // 緑色LEDを点灯しその他を消灯
-   digitalWrite(b, HIGH);
-   delay(wait);
+void blue(){
+  digitalWrite(LED_R, HIGH);
+   digitalWrite(LED_G, LOW);  // 緑色LEDを点灯しその他を消灯
+   digitalWrite(LED_B, HIGH);
+   delay(WAIT);
  
 }
-void green(int r, int g,int b,int wait){
-   digitalWrite(r, HIGH);
-   digitalWrite(g, HIGH);   // 青色LEDを点灯しその他を消灯
-   digitalWrite(b, LOW);
-   delay(wait);
+void green(){
+   digitalWrite(LED_R, HIGH);
+   digitalWrite(LED_G, HIGH);   // 青色LEDを点灯しその他を消灯
+   digitalWrite(LED_B, LOW);
+   delay(WAIT);
 }
- 
+
+void orange(){
+  digitalWrite(LED_R, LOW);
+   digitalWrite(LED_G, HIGH);   // 青色LEDを点灯しその他を消灯
+   digitalWrite(LED_B, LOW);
+   delay(WAIT);
+}
+void white(){
+  digitalWrite(LED_R, LOW);
+   digitalWrite(LED_G, LOW);   // 青色LEDを点灯しその他を消灯
+   digitalWrite(LED_B, LOW);
+   delay(WAIT);
+}
+
+void temp_LED(){
+  int val=analogRead(A0);
+  float cel =(float)val*500.0/1024.0-60.0;
+  if(cel<10)
+    blue();
+  else if(cel<20)
+    white();
+  else if(cel<30)
+    orange();
+  else
+    red();
+  cel2=cel;
+}
+
  void setup(){
+   Serial.begin(9600);
    pinMode(LED_R, OUTPUT);  // それぞれのピンを出力に設定
    pinMode(LED_G, OUTPUT);
    pinMode(LED_B, OUTPUT);
-  
 }
- 
- 
+
+
  void loop(){
-  red(LED_R,LED_G,LED_B,WAIT);
-  blue(LED_R,LED_G,LED_B,WAIT);
-  green(LED_R,LED_G,LED_B,WAIT);
+  temp_LED();
+  Serial.println(cel2);
 }
